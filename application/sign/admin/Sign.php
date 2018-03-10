3824257d404488c6b768b25cdc93edb68465f0b0
liutao <282813637@qq.com>
2018-03-10 21:51:34 +0800

    点名前台逻辑修改

--- application/sign/admin/Sign.php
+++ application/sign/admin/Sign.php
@@ -1,174 +1,143 @@
 <?php
-namespace app\yiban\admin;
+namespace app\sign\admin;
 
 use app\admin\controller\Admin;
-use app\common\builder\ZBuilder; // 引入ZBuilder
+use app\common\builder\ZBuilder;
 use think\Db;
 use app\yiban\model\BaseInfo as BaseModel;
-use app\yiban\model\Sign as SignModel;
 
 /**
  * 签到模块后台
  * @author yongyijiu, rewirte by Yang 2018.3.3
- * Rewirte Steps:
- * 1.原sign模块去除无用功能，整体迁移至yiban下
- * 2.前后台公用方法写入yiban/common.php下
- * 3.数据库视图重写为tp的视图模型
- * @package app\yiban\bkjw
  */
 class Sign extends Admin
 {
-
     public function config(){
-       return $this->moduleConfig();
+      return $this->moduleConfig();
     }
-
-
-
-
     //签到信息列表
     public function signList($group = 'tab1'){
-
-    	$order = $this->getOrder();
-    	$map = $this->getMap();
+      $order = $this->getOrder();
+      $map = $this->getMap();
       $taskId = $this -> getLastSignTask()['id'];
-
-        //用来获取表头下拉菜单的默认值，默认值设为上次晚点名
-        //存在一个问题，如果当前正在晚点名，可能希望看到的默认是当前的晚点名而非上次晚点名
-        if (empty($map) || !isset($map['task_id'])) {
-            $map['task_id'] = $taskId;
-        }
-    	//用视图通过stu_id获取学生信息，task_id获取点名信息
-        //存在一个问题，dp_yiban_base_info中，不存在admin_id，无法用视图选出每个管理员管理的学生
-        //添加了admin_id
-        $data_list = Db::view('sign_record')
-                    ->view('sign_task','id,title,adminid','sign_record.task_id=sign_task.id')
-                    ->view('yiban_base_info','class,sex,name,college,number,admin_id','sign_record.stu_id=yiban_base_info.number AND
-                        yiban_base_info.admin_id ='.UID)
-                    ->where($map)
-                    ->order($order)
-                    ->paginate();
-        $list_tab = [
-            'tab1' => ['title' => '签到信息列表', 'url' => url('signList', ['group' => 'tab1'])],
-            'tab2' => ['title' => '热力图统计', 'url' => url('signList', ['group' => 'tab2'])]
-        ];
-        //获取上次的晚点名之前的所有点名下拉选项
-        //注意这里要保证$task_id是自增的
-        $sign_task_list = Db::name('sign_task')->where('id','<=',$taskId)->column('id,title');
-
-        switch ($group) {
+      //用来获取表头下拉菜单的默认值，默认值设为上次晚点名
+      //存在一个问题，如果当前正在晚点名，可能希望看到的默认是当前的晚点名而非上次晚点名
+      if (empty($map) || !isset($map['task_id'])) {
+        $map['task_id'] = $taskId;
+      }
+      //用视图通过stu_id获取学生信息，task_id获取点名信息
+      //存在一个问题，dp_yiban_base_info中，不存在admin_id，无法用视图选出每个管理员管理的学生
+      //添加了admin_id
+      $data_list = Db::view('sign_record')
+          ->view('sign_task','id,title,adminid','sign_record.task_id=sign_task.id')
+          ->view('yiban_base_info','class,sex,name,college,number,admin_id','sign_record.stu_id=yiban_base_info.number AND
+                          yiban_base_info.admin_id ='.UID)
+          ->where($map)
+          ->order($order)
+          ->paginate();
+      $list_tab = [
+        'tab1' => ['title' => '签到信息列表', 'url' => url('signList', ['group' => 'tab1'])],
+        'tab2' => ['title' => '热力图统计', 'url' => url('signList', ['group' => 'tab2'])]
+      ];
+      //获取上次的晚点名之前的所有点名下拉选项
+      //注意这里要保证$task_id是自增的
+      $sign_task_list = Db::name('sign_task')->where('id','<=',$taskId)->column('id,title');
+      switch ($group) {
         case 'tab1':
-           return ZBuilder::make('table')
-                ->addColumns([
-                ['name', '姓名','', '', '', 'text-center'],
-                ['class', '班级','', '', '', 'text-center'],
-                ['number', '学号','', '', '', 'text-center'],
-                ['college', '学院','', '', '', 'text-center'],
-                ['latitude', '经度', '', '', '', 'text-center'],
-                ['longitude', '纬度', '', '', '', 'text-center'],
-                ['at_school', '位置', '', '', '', 'text-center'],
-                ['timestamp', '签到时间'],
-                ['title', '晚点名事项'],
-                ['right_button', '操作', 'btn']
-            ])
-                ->addRightButton('delete') // 添加删除按钮
-                ->addOrder('timestamp') // 添加排序
-                ->addTopSelect('task_id', '',$sign_task_list,$taskId)//添加顶部筛选
-                ->setRowList($data_list) // 设置表格数据
-                ->setTabNav($list_tab,  $group)
-                ->fetch();
-            break;
+        return ZBuilder::make('table')
+        ->addColumns([
+            ['name', '姓名','', '', '', 'text-center'],
+            ['class', '班级','', '', '', 'text-center'],
+            ['number', '学号','', '', '', 'text-center'],
+            ['college', '学院','', '', '', 'text-center'],
+            ['latitude', '经度', '', '', '', 'text-center'],
+            ['longitude', '纬度', '', '', '', 'text-center'],
+            ['at_school', '位置', '', '', '', 'text-center'],
+            ['timestamp', '签到时间'],
+            ['title', '晚点名事项'],
+            ['right_button', '操作', 'btn']
+          ])
+          ->addRightButton('delete') // 添加删除按钮
+          ->addOrder('timestamp') // 添加排序
+          ->addTopSelect('task_id', '',$sign_task_list,$taskId)//添加顶部筛选
+          ->setRowList($data_list) // 设置表格数据
+          ->setTabNav($list_tab,  $group)
+          ->fetch();
+        break;
         case 'tab2':
         //传递task_id的问题
-
-      //  dump($data_list);
-            $location_data = DB::name('sign_record')->field("latitude,longitude")->select();
-
-            return view('map',['location_data' => $location_data]);
-
-            break;
-          }
-
-    }
-
+        $location_data = DB::name('sign_record')->field("latitude,longitude")->select();
+          return view('map',['location_data' => $location_data]);
+          break;
+        }
+      }
     //签到率统计
     //目前暂时无法根据签到率排序
-    public function signCount($group = 'tab1'){
-
-        $order = $this->getOrder();
-        $map = $this->getMap();
-        $taskId = $this -> getLastSignTask()['id'];
-
-        //用来获取表头下拉菜单的默认值，默认值设为上次晚点名
-        if (empty($map) || !isset($map['task_id'])) {
-            $map['task_id'] = $taskId;
-        }
-
-        //获取上次之前所有的晚点名下拉选项
-        $sign_task_list = Db::name('sign_task')->where('id','<=',$taskId)->column('id,title');
-
-        //获取班级的列表信息同时获取一个学号用来回调得到班级号
-        $data = Db::view('sign_record')
-              ->view('sign_task','id,title,adminid','sign_record.task_id=sign_task.id')
-              ->view('yiban_base_info','class,sex,name,college,number,admin_id','sign_record.stu_id=yiban_base_info.number
+    public function signRateCount($group = 'tab1'){
+      $order = $this->getOrder();
+      $map = $this->getMap();
+      $taskId = $this -> getLastSignTask()['id'];
+      //用来获取表头下拉菜单的默认值，默认值设为上次晚点名
+      if (empty($map) || !isset($map['task_id'])) {
+        $map['task_id'] = $taskId;
+      }
+      //获取上次之前所有的晚点名下拉选项
+      $sign_task_list = Db::name('sign_task')->where('id','<=',$taskId)->column('id,title');
+      //获取班级的列表信息同时获取一个学号用来回调得到班级号
+      $data = Db::view('sign_record')
+        ->view('sign_task','id,title,adminid','sign_record.task_id=sign_task.id')
+        ->view('yiban_base_info','class,sex,name,college,number,admin_id','sign_record.stu_id=yiban_base_info.number
               AND  yiban_base_info.admin_id ='.UID)
-              ->group('class')
-              ->where($map)
-              ->order($order)
-              ->paginate();
-
-        $list_tab = [
-            'tab1' => ['title' => '班级签到率', 'url' => url('signCount', ['group' => 'tab1'])],
-            'tab2' => ['title' => '学院签到率', 'url' => url('signCount', ['group' => 'tab2'])],
-            'tab3' => ['title' => '年级签到率', 'url' => url('signCount', ['group' => 'tab3'])],
-        ];
-
-
-        switch ($group) {
+        ->group('class')
+        ->where($map)
+        ->order($order)
+        ->paginate();
+      $list_tab = [
+        'tab1' => ['title' => '班级签到率', 'url' => url('signCount', ['group' => 'tab1'])],
+        'tab2' => ['title' => '学院签到率', 'url' => url('signCount', ['group' => 'tab2'])],
+        'tab3' => ['title' => '年级签到率', 'url' => url('signCount', ['group' => 'tab3'])],
+      ];
+      switch ($group) {
         case 'tab1':
+        return ZBuilder::make('table')
+          ->addColumns([
+            ['class', '签到率','callback',function($value, $data){
+              return $this->getSignRateByClassId($value, $data['id']);
+            },'__data__'],
+            ['number','班级','callback',function($value){
+              return $this->getClassId($value);
+            }],
+            ['major','专业'],
+            ['college','学院'],
+            ['title','晚点名'],
+          ])
+          ->addTopSelect('task_id', '',$sign_task_list,$taskId)//添加顶部筛选
+          ->setTabNav($list_tab,  $group)
+          ->setRowList($data) // 设置表格数据
+          ->fetch();
+          break;
+          case 'tab2':
+          return ZBuilder::make('table')
+          ->addColumns([
+            ['class', '学院','', '', '', 'text-center'],
+            ['signRate', '签到率','', '', '', 'text-center'],
+          ])
+          ->setTabNav($list_tab,  $group)
+          ->fetch();
+          break;
+          case 'tab3':
             return ZBuilder::make('table')
-                ->addColumns([
-                ['class', '签到率','callback',function($value, $data){
-                    return $this->getSignRateByClassId($value, $data['id']);
-                },'__data__'],
-                ['number','班级','callback',function($value){
-                    return $this->getClassId($value);
-                }],
-                ['major','专业'],
-                ['college','学院'],
-                ['title','晚点名'],
-            ])
-                ->addTopSelect('task_id', '',$sign_task_list,$taskId)//添加顶部筛选
-                ->setTabNav($list_tab,  $group)
-
-                ->setRowList($data) // 设置表格数据
-                ->fetch();
-            break;
-        case 'tab2':
-            return ZBuilder::make('table')
-                ->addColumns([
-                ['class', '学院','', '', '', 'text-center'],
-                ['signRate', '签到率','', '', '', 'text-center'],
-
-            ])
-                ->setTabNav($list_tab,  $group)
-                ->fetch();
-            break;
-        case 'tab3':
-            return ZBuilder::make('table')
-                ->addColumns([
+              ->addColumns([
                 ['class', '年级','', '', '', 'text-center'],
                 ['signRate', '签到率','', '', '', 'text-center'],
-
-            ])
-                ->setTabNav($list_tab,  $group)
-                ->fetch();
-            break;
-          }
-    }
-
+              ])
+              ->setTabNav($list_tab,  $group)
+              ->fetch();
+          break;
+        }
+      }
     //特殊情况列表
-    public function signSpecial(){
+    public function signSpecialList(){
         $order = $this->getOrder();
         $map = $this->getMap();
         $taskId = $this -> getLastSignTask()['id'];
@@ -181,20 +150,20 @@ class Sign extends Admin
         $sign_task_list = Db::name('sign_task')->where('id','<=',$taskId)->column('id,title');
 
         //这个语句可以找出所有的班级但是由于目前数据库中的数据有限所以多数班级筛选出task_id为空
-        // $data = Db::view('yiban_base_info','major,class,sex,name,college,number,admin_id')
-        //       ->view('sign_record','stu_id,task_id,at_school,timestamp','sign_record.stu_id=yiban_base_info.number','LEFT')
-        //       ->view('sign_task','id,title,adminid','sign_record.task_id=sign_task.id','LEFT')
-        //       ->group('class')
-        //       ->order($order)
-        //       ->paginate();
-        $data = Db::view('sign_record')
-              ->view('sign_task','id,title,adminid','sign_record.task_id=sign_task.id')
-              ->view('yiban_base_info','class,sex,name,college,number,admin_id','sign_record.stu_id=yiban_base_info.number
-              AND  yiban_base_info.admin_id ='.UID)
+        $data = Db::view('yiban_base_info','major,class,sex,name,college,number,admin_id')
+              ->view('sign_record','stu_id,task_id,at_school,timestamp','sign_record.stu_id=yiban_base_info.number','LEFT')
+              ->view('sign_task','id,title,adminid','sign_record.task_id=sign_task.id','LEFT')
               ->group('class')
-              ->where($map)
               ->order($order)
               ->paginate();
+        // $data = Db::view('sign_record')
+        //       ->view('sign_task','id,title,adminid','sign_record.task_id=sign_task.id')
+        //       ->view('yiban_base_info','class,sex,name,college,number,admin_id','sign_record.stu_id=yiban_base_info.number
+        //       AND  yiban_base_info.admin_id ='.UID)
+        //       ->group('class')
+        //       ->where($map)
+        //       ->order($order)
+        //       ->paginate();
         //dump($data);
 
         return ZBuilder::make('table')
@@ -213,17 +182,15 @@ class Sign extends Admin
             ])
                 ->addColumn('right_button', '查看详情', 'btn')
                 ->addTopSelect('task_id', '',$sign_task_list,$taskId)//添加顶部筛选
-                ->addRightButton('edit',['href' => url('signSpecialList',['class_id' => '__class__','task_id'=> '__task_id__']),'title' => '详情','icon'=>'fa fa-fw fa-th-list'],'')
+                ->addRightButton('edit',['href' => url('signSpecialDetailList',['class_id' => '__class__','task_id'=> '__task_id__']),'title' => '详情','icon'=>'fa fa-fw fa-th-list'],'')
                 ->setRowList($data) // 设置表格数据
                 ->setPageTitle('特殊情况列表')
                 ->fetch();
     }
-
-
      /*
     * 获取签到率，分别有学院签到率，年级签到率，班级签到率
     */
-    public function getSignRateByClassId($class_id, $task_id){
+    private function getSignRateByClassId($class_id, $task_id){
 
         //统计一个班的签到人数
         $signCount = Db::view('SignRecord','id,task_id,stu_id')
@@ -243,7 +210,7 @@ class Sign extends Admin
     }
 
     //用来显示每个班级的没签到的人员的具体信息
-    public function signSpecialList($class_id, $task_id){
+    public function signSpecialDetailList($class_id, $task_id){
       $classList = $this -> getNotSignListByClassId($class_id, $task_id);
       return ZBuilder::make('table')
               ->addColumns([
@@ -295,7 +262,6 @@ class Sign extends Admin
         }
         return $classList;
     }
-
     /*
     * 2017-10-16 0:38 Yang
     * method 获取当前点名任务
@@ -347,7 +313,6 @@ class Sign extends Admin
         }
         return $task;
     }
-
     /*
     * 2017-10-16 0:38 Yang
     * method 获取当前时间上一次点名任务，截止当填晚上0:00:00，用来判断是否需要补签
@@ -367,7 +332,6 @@ class Sign extends Admin
             ->find();
         return $task;
     }
-
     /*
     * 根据学号获取管理员（辅导员）ID
     */
--- application/yiban/home/Sign.php
+++ application/yiban/home/Sign.php
@@ -5,6 +5,8 @@ use think\Log;
 use think\Db;
 use think\Session;
 
+use app\index\controller\Home;
+
 
 use app\yiban\model\BaseInfo as BaseModel;
 
@@ -12,7 +14,7 @@ use app\yiban\model\BaseInfo as BaseModel;
  * 易班签到控制器
  * @package app\yiban\home
  */
-class Sign extends Api
+class Sign extends Home
 {
     const SCHOOL_AREA = array(
         array( 'y' => 34.376459,'x' => 108.912002),
@@ -30,13 +32,18 @@ class Sign extends Api
 
 	public function init(){
 
-		$checkData = $this->checkParams();
-		if($checkData['status'] !== true){
-			return json($checkData);
-		}
+		//$checkData = $this->checkParams();
+		//if($checkData['status'] !== true){
+		//	return json($checkData);
+		//}
 
-        $stuInfo = $this->getStudentInfo($this->token);
-        
+      //  $stuInfo = $this->getStudentInfo($this->token);
+      $stuInfo = new \stdClass();
+      $stuInfo->yb_realname = '李强';
+      $stuInfo->yb_studentid = '2017901906';
+      $stuInfo->yb_userid = '1';
+
+
         if($stuInfo != false){
             $model = new BaseModel;
             $stuBaseInfo = $model->getBaseInfoById($stuInfo->yb_studentid);
@@ -45,8 +52,8 @@ class Sign extends Api
             $personalInitData['yb_realname'] = $stuInfo->yb_realname;
             $personalInitData['yb_id'] = $stuInfo->yb_userid;
             $personalInitData['stu_id'] = $stuInfo->yb_studentid;
-            
-            $personalInitData['head_img'] = $stuInfo->yb_userhead;
+
+            //$personalInitData['head_img'] = $stuInfo->yb_userhead;
             $personalInitData['sign_status'] = $this->getUserSignStatus($personalInitData['stu_id']);
 
             if($stuBaseInfo['major']){
@@ -58,20 +65,20 @@ class Sign extends Api
 
             $this->initData['personal'] = $personalInitData;
             $this->initData['public'] = $this->getSign($personalInitData['stu_id']);
-            return json($this->initData);
+            dump(json($this->initData));
         }else{
             $data['status'] = 'error';
             $data['code'] = '0x2004';
             $data['info'] = '无法获取易班信息，可能是未通过校方认证';
             return json($data);
         }
-    	
+
     }
 
     public function submit(){
 
         $token = $this->getToken($this->verifyRequest);
-        
+
         //申请校级权限后打开
         $stu_id = $this->getStudentId($token);
 
@@ -178,7 +185,7 @@ class Sign extends Api
 
             foreach ($classList as $key => $value) {
                 foreach ($signList as $k => $v) {
-                    
+
                     if($value['number'] == $v['number']){
                         unset($classList[$key]);
                     }
@@ -208,7 +215,7 @@ class Sign extends Api
         }
 
         return json($data);
-        
+
     }
 
     public function me(){
@@ -253,7 +260,7 @@ class Sign extends Api
     * return bool(true or false)
     */
     private function recordSignData($stu_id,$latitude,$longitude,$task_id){
-        
+
         $point['x'] = $latitude;
         $point['y'] = $longitude;
         //bool(true or false)
@@ -321,9 +328,10 @@ class Sign extends Api
     /*
     * 获取当前点名任务及对应通知
     */
-    private function getSign($stu_id){
+    //private
+    public function getSign(){
+        $stu_id
         $signData = array();
-
         $sign = $this->getSignTask($stu_id);
         if($sign['task_status'] == 0){
             $signData = $sign;
@@ -336,7 +344,7 @@ class Sign extends Api
             $signData['sms_verify'] = $sign['sms_verify'];
             $signData['task_status'] = $sign['task_status'];
         }
-        
+
         return $signData;
     }
 
@@ -347,45 +355,47 @@ class Sign extends Api
     * return $task: 点名人数数组（title,start_time,end_time,adminid,sort,status）
     */
     private function getSignTask($stu_id,$last = false){
-
         $time = time();
         $todayStartTime = mktime(0,0,0,date("m",$time),date("d",$time),date("Y",$time));
         $todayEndTime = mktime(23,59,59,date("m",$time),date("d",$time),date("Y",$time));
         $adminId = $this->getStuAdminId($stu_id);
-
-        //1.当前时间在任务开始-结束区间内
-        $task = Db::table('dp_sign_task')
-            ->where('start_time','<',$time)
-            ->where('end_time','>',$time)
-            ->where('status',1)
-            ->where('adminid',$adminId)
-            //排序1：sort重要性排序，排序2：选择开始时间晚的
-            ->order('sort DESC,start_time DESC')
-            ->find();
-        if($task){
-            $task['task_status'] = 1;
-            $task['msg'] = '当前时间存在签到任务';
+        //先来判断今天开始到此刻有没有未签到的点名
+        $result = $this -> isHavePastTask($stu_id);
+        if ($result !== true) {
+          $task =  $result;
+          $task['task_status'] = 3;
+          $task['msg'] = '你有任务未签到，请进行补签';
         }else{
-            //2.当前时间不在任务开始-区间内
-            $task = Db::table('dp_sign_task')
-                ->where('end_time','<',$todayEndTime)
-                ->where('start_time','>',$todayStartTime)
-                ->order('sort DESC,start_time DESC')
-                ->find();
-            if(empty($task)){
+          //1.当前时间在任务开始-结束区间内
+          $task = Db::table('dp_sign_task')
+              ->where('start_time','<',$time)
+              ->where('end_time','>',$time)
+              ->where('status',1)
+              ->where('adminid',$adminId)
+              //排序1：sort重要性排序，排序2：选择开始时间晚的
+              ->order('sort DESC,start_time DESC')
+              ->find();
+          if($task){
+              $task['task_status'] = 1;
+              $task['msg'] = '正在进签到';
+          }else{
+              //2.当前时间不在任务开始-区间内,寻找接下来还有没有任务
+              $task = Db::table('dp_sign_task')
+                  ->where('start_time','>',$time)
+                  ->where('end_time','<',$todayEndTime)
+                  ->order('start_time ASC,id ASC')
+                  ->find();
+              if (empty($task)) {
                 $task['task_status'] = 0;
                 $task['msg'] = '当前无签到任务';
-            }elseif($time < $task['start_time']){
-                //当天存在任务，且当前时间小于start_time，即点名未开始(预告)
+              }else {
+                echo "下次点名还没到，进行预告";
+                //即点名未开始(预告)
                 $task['task_status'] = 2;
                 $task['msg'] = '即将开始签到';
-            }elseif($time >= $task['end_time']){
-                //当前存在任务，且当前时间大于end_time，即点名已经结束（补签）
-                $task['task_status'] = 3;
-                $task['msg'] = '签到已经结束';
+              }
             }
-
-        }            
+        }
         return $task;
     }
 
@@ -404,8 +414,8 @@ class Sign extends Api
     /*
     * 2017-10-16 0:38 Yang
     * method 获取当前时间上一次点名任务，截止当填晚上0:00:00，用来判断是否需要补签
-    * params 
-    * return 
+    * params
+    * return
     */
     private function getLastSignTask($stu_id){
         $time = time();
@@ -426,7 +436,62 @@ class Sign extends Api
         }
 
     }
+    /*
+    * 用来获取距离当前时间最近的下次当天签到任务
+    */
+    public function getTodayNextTask(){
+      $stu_id = "2017902148";
+      $time = time();
+      $todayStartTime = mktime(0,0,0,date("m",$time),date("d",$time),date("Y",$time));
+      $todayEndTime = mktime(23,59,59,date("m",$time),date("d",$time),date("Y",$time));
+      $adminId = $this->getStuAdminId($stu_id);
+      $task = Db::table('dp_sign_task')
+          ->where('start_time','>',$time)
+          ->where('end_time','<',$todayEndTime)
+          ->order('start_time ASC,id ASC')
+          ->find();
+      if (empty($task)) {
+        echo "当天没有后续的签到任务了";
+      }else {
+        echo "下次点名还没到，进行预告";
+      }
+    }
 
+    /*
+    * 用来判断当天是否有已经过去的签到任务
+    */
+    public function isHavePastTask(){
+      $stu_id = "2017902148";
+      $time = time();
+      $todayStartTime = mktime(0,0,0,date("m",$time),date("d",$time),date("Y",$time));
+      $todayEndTime = mktime(23,59,59,date("m",$time),date("d",$time),date("Y",$time));
+      $adminId = $this->getStuAdminId($stu_id);
+      $task = Db::table('dp_sign_task')
+          ->where('end_time','<',$time)
+          ->where('start_time','>',$todayStartTime)
+          ->order('start_time Desc,id DESC')
+          ->find();
+      //dump($task);
+      if(empty($task)){
+          //当天到此刻还没有签到任务
+          return true;
+      }else {
+        //要判断该学生是否签到
+        $signRecord = Db::table('dp_sign_record')
+            ->where('task_id',$task['id'])
+            ->where('stu_id',$stu_id)
+            ->find();
+        //dump($signRecord);
+        if(empty($signRecord)){
+
+          //dump($task);
+          return $task;
+        }else{
+          //不存在需要补签的任务;
+          return true;
+        }
+      }
+    }
 
     /*
     * 获取当前点名对应通知
@@ -457,7 +522,7 @@ class Sign extends Api
             $signNotice['now_page'] = $page;
             $signNotice['total_page'] = (int)ceil($totalPage);
             $signNotice['rows_page'] = 10;
-        }   
+        }
 
         return $signNotice;
     }
@@ -509,26 +574,26 @@ class Sign extends Api
         $py = $lnglat['y'];
     // echo $count.' '.$px.' '.$py;
         $flag = false;
-    
-        for ($i = 0, $j = $count - 1; $i < $count; $j = $i, $i++) { 
-            $sy = $polygon[$i]['y']; 
+
+        for ($i = 0, $j = $count - 1; $i < $count; $j = $i, $i++) {
+            $sy = $polygon[$i]['y'];
             $sx = $polygon[$i]['x'];
             $ty = $polygon[$j]['y'];
             $tx = $polygon[$j]['x'];
-    
+
             if ($px == $sx && $py == $sy || $px == $tx && $py == $ty){
                 return true;
             }
-                    
+
             if ($sy < $py && $ty >= $py || $sy >= $py && $ty < $py) {
                 $x = $sx + ($py - $sy) * ($tx - $sx) / ($ty - $sy);
                 if ($x == $px){
                     return true;
                 }
-                    
+
                 if ($x > $px){
                     $flag = !$flag;
-                }     
+                }
             }
         }
         return $flag;
